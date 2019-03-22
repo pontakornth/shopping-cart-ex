4a54e17200f546bbea4333713526d6dd3905ad79
pontakornth <pontakorn61@gmail.com>
2019-03-22 15:59:10 +0700

    Add remove item functionality

--- components/Cart.vue
+++ components/Cart.vue
@@ -8,7 +8,7 @@
       </div>
     </div>
     <div v-else>
-      <span v-for="item in allItems" :key="item.id" class="media">
+      <span v-for="(item, index) in allItems" :key="index" class="media">
         <span class="media-left">
           <figure class="image is-64x64">
             <img :src="item.imageUrl" :alt="item.name" />
@@ -16,11 +16,36 @@
         </span>
         <span class="media-content">
           <h6 class="title is-6">{{ item.name }}</h6>
+          <div class="level is-mobile">
+            <div class="level-left">
+              <div class="level-item">
+                <a
+                  class="button is-small is-danger"
+                  @click="removeItemFromCart(item)"
+                  >Remove item
+                </a>
+              </div>
+            </div>
+          </div>
         </span>
       </span>
       <span class="media">
         <span class="media-content">
-          <p>Total price : {{ totalPrice }}</p>
+          <div class="level">
+            <div class="level-left">
+              <div class="level-item">
+                <p>Total price : {{ totalPrice }}</p>
+              </div>
+            </div>
+            <div class="level-right">
+              <div class="level-item">
+                <a class="button is-medium is-info">
+                  <b-icon pack="fas" size="is-small" icon="shopping-cart" />
+                  <span>Clear cart</span>
+                </a>
+              </div>
+            </div>
+          </div>
         </span>
       </span>
     </div>
@@ -28,13 +53,18 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 export default {
   computed: {
     ...mapGetters({
       allItems: 'getItemsInCart',
       totalPrice: 'getTotalPriceInCart',
       isCartEmpty: 'isCartEmpty'
+    })
+  },
+  methods: {
+    ...mapActions({
+      removeItemFromCart: 'removeItemFromCart'
     })
   }
 }
